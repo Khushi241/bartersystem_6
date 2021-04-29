d9660758effdebc8918600bcf5481cedae92f88f
Khushi <60261630+Khushi241@users.noreply.github.com>
2021-04-30 00:45:21 +0530

    Add files via upload

---
+++ screens/ExchangeScreen.js
@@ -0,0 +1,78 @@
+import React,{Component} from 'react';
+import {View,Text,TextInput} from 'react-native';
+
+
+export default class ExchangeScreen extends Component{
+    constructor(){
+        super()
+        this.state={
+            userId:firebase.auth().currentUser.email,
+            itemName:'',
+            itemDescription:''
+        }
+        
+    }
+
+    createUniqueId(){
+        return Math.random().toString(36).substring(7)
+    }
+    
+    addItem=(itemName,itemDescription)=>{
+        var userId=this.state.userId
+        db.collection('RequestItem').add({
+            "user_Id":userId,
+            "itemName":itemName,
+            "itemDescription":itemDescription,
+
+        })
+        this.setState({
+            itemName:'',
+            itemDescription:''
+        })
+
+        return Alert.alert(
+            'Item ready to exchange',
+            '',
+            [
+                {text: 'OK', onPress: ()=>{
+                    this.props.navigation.navigate('HomeScreen')
+                }}
+            ]
+        )
+    }
+    
+
+    render(){
+        return(
+
+            <View>
+                <TextInput
+                placeholder={"Item Name"}
+                onChangeText={(text)=>{
+                    this.setState({itemName:text})
+                }}
+                value={this.state.itemName}
+                ></TextInput>
+
+                <TextInput
+                placeholder={"Description"}
+                onChangeText={(text)=>{
+                    this.setState({itemDescription:text})
+                }}
+                value={this.state.itemDescription}
+                ></TextInput>
+                <TouchableOpacity
+                style={{
+                    width:100,
+                    height:30
+                }}
+                onPress={()=>{this.addItem(this.state.itemName,this.state.itemDescription)}}
+                >
+                    <Text>Add Item</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+}
+
